83acffd41cb7331ff63d76299b67b64df9117b72
Ahmet <demirahmetfaruk@gmail.com>
2026-04-24 23:30:30 +0300

    fix: ultimate cors bypass

--- server/index.js
+++ server/index.js
@@ -13,14 +13,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: [
-        'https://block-realm-three.vercel.app', 
-        'http://localhost:5173',                
-        process.env.CLIENT_URL                  
-    ],
+    origin: function (origin, callback) {
+        callback(null, true);
+    },
     credentials: true
 }));
-
 
 app.head('/', (req, res) => {
     res.status(200).end();
